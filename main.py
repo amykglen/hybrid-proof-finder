2ef934d975673cc1c87bfd02dcefbe2f87eb5cf3
amykglen <glena@oregonstate.edu>
2022-03-15 22:10:20 -0700

    Most of code there; still bugs, namely with determining end

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import copy
+import itertools
+import time
 from collections import defaultdict
+from typing import List, Optional, Tuple, Dict, Set
 
 import graphviz
 
@@ -7,9 +11,7 @@ RANDOM_NO_REPLACE = "$-"
 G = "G"
 F = "F"
 E = "E"
-x = "x"
 XOR = "XOR"
-OUTPUT = "OUT"
 WILDCARD = "*"
 
 
@@ -55,11 +57,19 @@ class Graph:
         self.nodes[key] = Node(key, kind, input_rank, output_rank, color, style)
 
     def add_edge(self, source_key: str, target_key: str):
-        key = f"e:{source_key}--{target_key}"
+        key = f"{source_key}--{target_key}"
         assert key not in self.edges
         assert source_key in self.nodes and target_key in self.nodes
         color = "red" if self.nodes[source_key].kind == RANDOM_NO_REPLACE else "black"
         self.edges[key] = Edge(key, source_key, target_key, color)
+
+    def remove_node(self, node_key: str):
+        if node_key in self.nodes:
+            del self.nodes[node_key]
+        edge_keys_to_delete = [edge.key for edge in self.edges.values()
+                               if edge.source_key == node_key or edge.target_key == node_key]
+        for edge_key in edge_keys_to_delete:
+            del self.edges[edge_key]
 
     def print(self):
         for node in self.nodes.values():
@@ -82,6 +92,35 @@ class Graph:
         dot.render(f"{self.name}.gv", view=True)
         # TODO: Try to pin all input nodes at same top layer and output nodes at same bottom layer?
 
+    def validate(self):
+        inputs = [node.input_rank for node in self.nodes.values() if node.input_rank]
+        outputs = [node.output_rank for node in self.nodes.values() if node.output_rank]
+        assert inputs or outputs
+        return inputs, outputs
+
+    def change_node_key(self, old_key: str, new_key: str):
+        # Delete the old node
+        assert old_key in self.nodes
+        node = self.nodes[old_key]
+        del self.nodes[old_key]
+
+        # Add node under new key
+        assert new_key not in self.nodes
+        node.key = new_key
+        self.nodes[new_key] = node
+
+        # Then edit any attached edges
+        edge_keys_to_modify = {edge.key for edge in self.edges.values()
+                               if edge.source_key == old_key or edge.target_key == old_key}
+        for edge_key in edge_keys_to_modify:
+            edge = self.edges[edge_key]
+            if edge.source_key == old_key:
+                edge.source_key = new_key
+            elif edge.target_key == old_key:
+                edge.target_key = new_key
+            self.edges[f"{edge.source_key}--{edge.target_key}"] = edge
+            del self.edges[edge_key]
+
 
 class IndistinguishablePair:
 
@@ -92,15 +131,10 @@ class IndistinguishablePair:
 
     def validate(self):
         # Make sure there are matching numbers of inputs/outputs in the two graphs
-        inputs_a = sorted([node.input_rank for node in self.graph_a.nodes.values() if node.input_rank])
-        inputs_b = sorted([node.input_rank for node in self.graph_b.nodes.values() if node.input_rank])
-        assert inputs_a == inputs_b
-        outputs_a = sorted([node.output_rank for node in self.graph_a.nodes.values() if node.output_rank])
-        outputs_b = sorted([node.output_rank for node in self.graph_b.nodes.values() if node.output_rank])
-        assert outputs_a == outputs_b
-
-        # Make sure at least some inputs or outputs are specified
-        assert inputs_a or outputs_a
+        inputs_a, outputs_a = self.graph_a.validate()
+        inputs_b, outputs_b = self.graph_b.validate()
+        assert sorted(inputs_a) == sorted(inputs_b)
+        assert sorted(outputs_a) == sorted(outputs_b)
 
         # Make sure all 'ranks' are unique
         assert len(set(inputs_a)) == len(inputs_a)
@@ -191,28 +225,166 @@ def create_standard_rules():
     rule_6.graph_b.add_node("in", WILDCARD, input_rank=1)
     rule_6.graph_b.add_node("$", RANDOM, output_rank=1)
 
-    return [rule_1, rule_2, rule_3, rule_4, rule_5, rule_6]
+    # return [rule_1, rule_2, rule_3, rule_4, rule_5, rule_6]
+    return [rule_3]
+
+
+def get_adjacency_dict(graph: Graph) -> defaultdict:
+    adjacency_dict = defaultdict(set)
+    for edge in graph.edges.values():
+        adjacency_dict[edge.source_key].add(edge.target_key)
+    return adjacency_dict
+
+
+def standardize_keys(adjacency_dict: Dict[str, Set[str]], key_map: Dict[str, int]) -> dict:
+    adj_dict_standardized = dict()
+    for node_key, neighbors in adjacency_dict.items():
+        standardized_node_key = key_map[node_key]
+        neighbors_standardized = {key_map[neighbor_key] for neighbor_key in neighbors}
+        adj_dict_standardized[standardized_node_key] = neighbors_standardized
+    return adj_dict_standardized
+
+
+def has_subgraph(graph: Graph, template: Graph):
+    # Create some helper maps
+    nodes_by_kind = defaultdict(set)  # TODO: Add all to wildcard kind
+    for node in graph.nodes.values():
+        nodes_by_kind[node.kind].add(node.key)
+    template_nodes = list(template.nodes.values())
+    template_adj_dict = get_adjacency_dict(template)
+    template_key_to_index_map = {node.key: index for index, node in enumerate(template_nodes)}
+    standardized_template_adj_dict = standardize_keys(template_adj_dict, template_key_to_index_map)
+
+    # Create possible combinations of nodes that could be used to fulfill the template (don't check edges yet)
+    node_lists = [nodes_by_kind[node.kind] for node in template_nodes]
+    combinations_raw = list(itertools.product(*node_lists))
+    # Get rid of any that use the same node for more than one template node
+    combinations = [combination for combination in combinations_raw if len(set(combination)) == len(combination)]
+
+    # Add edges and figure out if we have any subgraphs matching the template
+    matching_subgraphs = []
+    for combination_node_keys in combinations:
+        combination_graph = Graph(name="--".join(combination_node_keys))
+
+        combination_graph.nodes = {node_key: graph.nodes[node_key] for node_key in combination_node_keys}
+        combination_graph.edges = {edge_key: edge for edge_key, edge in graph.edges.items()
+                                   if edge.source_key in combination_graph.nodes and
+                                   edge.target_key in combination_graph.nodes}
+        combination_graph_adj_dict = get_adjacency_dict(combination_graph)
+        # Convert to standardized IDs so we can compare to template
+        index_to_comb_key_map = {index: node_key for index, node_key in enumerate(combination_node_keys)}
+        comb_key_to_index_map = {node_key: index for index, node_key in enumerate(combination_node_keys)}
+        template_to_comb_key_map = {template_key: index_to_comb_key_map[index]
+                                    for template_key, index in template_key_to_index_map.items()}
+        standardized_combination_adj_dict = standardize_keys(combination_graph_adj_dict, comb_key_to_index_map)
+        if standardized_template_adj_dict == standardized_combination_adj_dict:
+            matching_subgraphs.append([combination_graph, template_to_comb_key_map])
+    return matching_subgraphs
+
+
+def has_reached_end_state(graph_path: list, end: Graph) -> bool:
+    latest_graph = graph_path[-1][0]
+    latest_graph.print_fancy()
+    end.print_fancy()
+    return True if has_subgraph(latest_graph, end) else False
+
+
+def find_proof(start: Graph, end: Graph, rules: List[IndistinguishablePair]):
+    graph_paths = [[(copy.deepcopy(start), "-")]]
+    count = 0
+
+    while not any(has_reached_end_state(graph_path, end) for graph_path in graph_paths) and count < 5:
+        count += 1
+        print(f"starting while loop; have {len(graph_paths)} graph paths")
+        print(f"length of the graph path is {len(graph_paths[0])}")
+        extended_graph_paths = list()
+        for graph_path in graph_paths:
+            latest_graph_tuple = graph_path[-1]
+            latest_graph = latest_graph_tuple[0]
+            for rule in rules:
+                input_nodes_a = [node for node in rule.graph_a.nodes.values() if node.input_rank]
+                output_nodes_a = [node for node in rule.graph_a.nodes.values() if node.output_rank]
+
+                matching_subgraphs_a = has_subgraph(latest_graph, rule.graph_a)
+                for matching_subgraph, template_to_graph_key_map in matching_subgraphs_a:
+
+                    # Initiate a fresh copy of the subgraph that'll be swapped in (so we can modify it)
+                    subgraph_to_swap_in = copy.deepcopy(rule.graph_b)
+
+                    # Find corresponding input nodes and make the new subgraph uses the original graph's IDs for these
+                    for input_node_a in input_nodes_a:
+                        corresponding_input_node_b = next(node for node in subgraph_to_swap_in.nodes.values()
+                                                          if node.input_rank == input_node_a.input_rank)
+                        corresponding_graph_key = template_to_graph_key_map[input_node_a.key]
+                        subgraph_to_swap_in.change_node_key(corresponding_input_node_b.key, corresponding_graph_key)
+                    # Find corresponding output nodes and make the new subgraph uses the original graph's IDs for these
+                    for output_node_a in output_nodes_a:
+                        corresponding_output_node_b = next(node for node in subgraph_to_swap_in.nodes.values()
+                                                           if node.output_rank == output_node_a.output_rank)
+                        corresponding_graph_key = template_to_graph_key_map[output_node_a.key]
+                        subgraph_to_swap_in.change_node_key(corresponding_output_node_b.key, corresponding_graph_key)
+
+                    # Make sure non input/output nodes in new subgraph have IDs not already used in larger graph
+                    non_io_node_keys_b = {node.key for node in rule.graph_b.nodes.values()
+                                          if not node.input_rank and not node.output_rank}
+                    for node_key in non_io_node_keys_b:
+                        subgraph_to_swap_in.change_node_key(node_key, str(time.time()))
+
+                    # TODO: Do the equivalent but for b to a, rather than only a to b
+
+                    # Remove non input/output nodes in the subgraph from the larger graph
+                    new_latest_graph = copy.deepcopy(latest_graph)
+                    io_template_node_keys = {node.key for node in input_nodes_a}.union({node.key for node in output_nodes_a})
+                    non_io_template_node_keys = set(rule.graph_a.nodes).difference(io_template_node_keys)
+                    non_io_graph_keys = {template_to_graph_key_map[template_key] for template_key in non_io_template_node_keys}
+                    for non_io_graph_key in non_io_graph_keys:
+                        new_latest_graph.remove_node(non_io_graph_key)
+
+                    # Delete input/output nodes from larger graph (but keep their edges) since they're in the new subgraph
+                    io_graph_keys = {template_to_graph_key_map[template_key] for template_key in io_template_node_keys}
+                    for io_graph_key in io_graph_keys:
+                        del new_latest_graph.nodes[io_graph_key]
+
+                    # Add the (now prepped) equivalent graph b into the larger graph
+                    new_latest_graph.nodes.update(subgraph_to_swap_in.nodes)
+                    new_latest_graph.edges.update(subgraph_to_swap_in.edges)
+
+                    # TODO: Clear output ranks just before the above to avoid confusion?
+
+                    extended_path = copy.deepcopy(graph_path)
+                    extended_path.append((new_latest_graph, rule.name))
+                    extended_graph_paths.append(extended_path)
+
+        if extended_graph_paths:
+            graph_paths = extended_graph_paths
 
 
 rules = create_standard_rules()
 
-for rule in rules:
-    rule.validate()
-    # rule.graph_a.print_fancy()
-    # rule.graph_b.print_fancy()
+start = Graph("start")
+start.add_node("in", WILDCARD, input_rank=1)
+start.add_node("$", RANDOM)
+start.add_node("xor", XOR)
+start.add_node("F", F)
+start.add_node("out1", WILDCARD, output_rank=1)
+start.add_node("out2", WILDCARD, output_rank=2)
+start.add_edge("in", "xor")
+start.add_edge("$", "xor")
+start.add_edge("xor", "F")
+start.add_edge("$", "out1")
+start.add_edge("F", "out2")
+start.validate()
+
+end = Graph("end")
+end.add_node("in", WILDCARD, input_rank=1)
+end.add_node("$1", RANDOM)
+end.add_node("$2", RANDOM)
+end.add_node("out1", WILDCARD, output_rank=1)
+end.add_node("out2", WILDCARD, output_rank=2)
+end.add_edge("$1", "out1")
+end.add_edge("$2", "out2")
+end.validate()
+
+find_proof(start, end, rules)
 
 
-# my_graph = Graph("test")
-# my_graph.add_node("n0i", "m", is_input=True)
-# my_graph.add_node("n0", "$")
-# my_graph.add_node("n1", "XOR")
-# my_graph.add_node("n1.5", "F")
-# my_graph.add_node("n2", "output", is_output=True)
-# my_graph.add_node("n3", "output", is_output=True)
-# my_graph.add_edge("n0", "n1")
-# my_graph.add_edge("n0i", "n1")
-# my_graph.add_edge("n1", "n2")
-# my_graph.add_edge("n1", "n1.5")
-# my_graph.add_edge("n1.5", "n3")
-# my_graph.print()
-# my_graph.print_fancy()
